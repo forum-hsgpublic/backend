ad4663acaed76cdfa08f3be6c546d111d582478b
hsgpublic <hsgpublic@gmail.com>
2024-11-03 09:40:55 +0900

    Serializers 정의

---
+++ forum/serializers.py
@@ -0,0 +1,33 @@
+# forum/serializers.py
+from rest_framework import serializers
+from .models import User, Category, Post, Comment, Tag, PostTag
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['user_id', 'login_id', 'email', 'join_date']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['category_id', 'name', 'description']
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['post_id', 'title', 'content', 'created_date', 'last_updated_date', 'user', 'category']
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['comment_id', 'content', 'created_date', 'post', 'user']
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['tag_id', 'name']
+
+class PostTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostTag
+        fields = ['post', 'tag']
