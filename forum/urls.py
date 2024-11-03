a7e749df0c9ea017d804628260dbe5805c910905
hsgpublic <hsgpublic@gmail.com>
2024-11-04 01:15:47 +0900

    Routing 수정

--- forum/urls.py
+++ forum/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # forum/urls.py
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, CategoryViewSet, PostViewSet, CommentViewSet, TagViewSet, PostTagViewSet
@@ -28,5 +29,6 @@ router.register(r'tags', TagViewSet)
 router.register(r'post_tags', PostTagViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
