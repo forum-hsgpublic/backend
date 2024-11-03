8bf88ae290396dbeacee51d915f5e6645cd9e6a1
hsgpublic <hsgpublic@gmail.com>
2024-11-04 08:33:29 +0900

    Router에 토큰 인증 URL 추가

--- forum/urls.py
+++ forum/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import UserViewSet, CategoryViewSet, PostViewSet, CommentViewSet, TagViewSet, PostTagViewSet
 
 router = DefaultRouter()
@@ -31,4 +32,6 @@ router.register(r'post_tags', PostTagViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
