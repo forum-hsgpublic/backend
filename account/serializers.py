581dd41cb4840200165f80590d24691c17c9e028
hsgpublic <hsgpublic@gmail.com>
2024-11-07 20:54:20 +0900

    User serializer를 account에 복사

---
+++ account/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'user_id',
+            'login_id',
+            'email',
+            'join_date',
+            'last_login',
+            'is_superuser',
+            'is_active',
+            'is_staff'
+        ]
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['login_id', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            login_id=validated_data['login_id'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
