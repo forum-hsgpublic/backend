c6cfb49d33b17f48cef8b9f94843d1e45a6bc7eb
hsgpublic <hsgpublic@gmail.com>
2024-11-07 20:37:48 +0900

    User 모델을 account에 복사

--- account/models.py
+++ account/models.py
@@ -1,3 +1,43 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+# 사용자 관리자 정의
+class UserManager(BaseUserManager):
+    def create_user(self, login_id, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        
+        email = self.normalize_email(email)
+        user = self.model(login_id=login_id, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, login_id=None, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        
+        return self.create_user(login_id, email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    login_id = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    join_date = models.DateTimeField(auto_now_add=True)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'login_id'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        db_table = 'users'
