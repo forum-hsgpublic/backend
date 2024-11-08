bcd6a556ea00bb070c5a74b9822b922813d8ea2d
hsgpublic <hsgpublic@gmail.com>
2024-11-08 19:59:18 +0900

    Forum models에서 user model 제거

--- forum/models.py
+++ forum/models.py
@@ -1,47 +1,7 @@
 # forum/models.py
 import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
-# 사용자 관리자 정의
-class UserManager(BaseUserManager):
-    def create_user(self, login_id, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("Users must have an email address")
-        
-        email = self.normalize_email(email)
-        user = self.model(login_id=login_id, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, login_id=None, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-        
-        return self.create_user(login_id, email, password, **extra_fields)
-
-class User(AbstractBaseUser, PermissionsMixin):
-    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    login_id = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(max_length=255, unique=True)
-    join_date = models.DateTimeField(auto_now_add=True)
-    is_superuser = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'login_id'
-    REQUIRED_FIELDS = ['email']
-
-    class Meta:
-        db_table = 'users'
+from django.contrib.auth import get_user_model
 
 class Category(models.Model):
     category_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -57,7 +17,7 @@ class Post(models.Model):
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     last_updated_date = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
     class Meta:
@@ -68,7 +28,7 @@ class Comment(models.Model):
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'comments'
